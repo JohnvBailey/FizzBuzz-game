d41603237a5492333793992986d0c6fbcf147b8d
John B <bohnjailey56@gmail.com>
2019-05-24 12:00:14 -0600

    add initial state management for run/pause.

--- app/src/main/java/edu/cnm/deepdive/fizzbuzz/SettingsActivity.java
+++ app/src/main/java/edu/cnm/deepdive/fizzbuzz/SettingsActivity.java
@@ -3,6 +3,8 @@ package edu.cnm.deepdive.fizzbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SeekBarPreference;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,17 +14,23 @@ public class SettingsActivity extends AppCompatActivity {
     setContentView(R.layout.activity_settings);
     if (savedInstanceState == null) {
       getSupportFragmentManager().beginTransaction()
-          .replace(R.id.settings_fragment_container, new Settingsfragment())
+          .replace(R.id.settings_fragment_container, new SettingsFragment())
           .commit();
     }
-
   }
 
-  public static class Settingsfragment extends PreferenceFragmentCompat {
+  public static class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-    setPreferencesFromResource(R.xml.preferences,rootKey);
+      setPreferencesFromResource(R.xml.preferences, rootKey);
+      PreferenceScreen screen =getPreferenceScreen();
+      SeekBarPreference numDigits =
+          (SeekBarPreference) screen.findPreference(getString(R.string.num_digits_key));
+      numDigits.setMin(1);
+
     }
+
   }
+
 }
